9ae1648e4e46a816c63ce1f6ead48a7fbde30fd7
felipepresas <felipe.presas@gmail.com>
2020-12-15 00:36:43 +0100

    15122020037

---
+++ 05_QuickClick/Assets/_Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class GameManager : MonoBehaviour
+{
+    public List<GameObject>targetPrefabs; // esto es hacerlo con una lista es como un array pero se ajusta
+    //public GameObject[] targetPrefabs2; esto es hacerlo con un array
+    public float spawnRate=1.0f;
+    void Start()
+    {
+        StartCoroutine(routine:SpawnTarget());
+    }
+
+  IEnumerator SpawnTarget()
+  {
+      while (true)
+      {
+          yield return new WaitForSeconds(spawnRate);
+          int idx=Random.Range(0,targetPrefabs.Count);
+          Instantiate(targetPrefabs[idx]);
+      }
+  }    void Update()
+    {
+        
+    }
+}
