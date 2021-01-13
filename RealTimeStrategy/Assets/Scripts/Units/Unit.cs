fa7368d031d040c0d21fe7c05a46d777a4fb8d2d
felipepresas <felipe.presas@gmail.com>
2021-01-14 00:28:46 +0100

    140120210028

--- RealTimeStrategy/Assets/Scripts/Units/Unit.cs
+++ RealTimeStrategy/Assets/Scripts/Units/Unit.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Mirror;
@@ -6,21 +7,54 @@ using UnityEngine.Events;
 
 public class Unit : NetworkBehaviour
 {
+    [SerializeField] private UnitMovement unitMovement = null;
     [SerializeField] private UnityEvent OnSelected = null;
     [SerializeField] private UnityEvent OnDeselected = null;
+
+    public static event Action<Unit> ServerOnUnitSpawned;
+    public static event Action<Unit> ServerOnUnitDespawned;
+    public static event Action<Unit> AuthorityOnUnitSpawned;
+    public static event Action<Unit> AuthorityOnUnitDespawned;
+
+    public UnitMovement GetUnitMovement()
+    {
+        return unitMovement;
+    }
+    #region Server
+
+    public override void OnStartServer()
+    {
+        ServerOnUnitSpawned?.Invoke(this);
+    }
+    public override void OnStopServer()
+    {
+         ServerOnUnitDespawned?.Invoke(this);
+    }
+
+    #endregion
 
     #region Client
     [Client]
     public void Select()
     {
-        if (!hasAuthority){return;}
+        if (!hasAuthority) { return; }
         OnSelected?.Invoke();
     }
     [Client]
     public void Deselect()
     {
-        if (!hasAuthority){return;}
+        if (!hasAuthority) { return; }
         OnDeselected?.Invoke();
+    }
+    public override void OnStartClient()
+    {
+        if(!isClientOnly || !hasAuthority){return;}
+        AuthorityOnUnitSpawned?.Invoke(this);
+    }
+    public override void OnStopClient()
+    {
+        if(!isClientOnly || !hasAuthority){return;}
+        AuthorityOnUnitDespawned?.Invoke(this);
     }
     #endregion
 }
--- RealTimeStrategy/Assets/Scripts/Units/UnitMovement.cs
+++ RealTimeStrategy/Assets/Scripts/Units/UnitMovement.cs
@@ -1,47 +1,29 @@
-using System.Collections;
-using System.Collections.Generic;
 using Mirror;
 using UnityEngine;
 using UnityEngine.AI;
-using UnityEngine.InputSystem;
 
 public class UnitMovement : NetworkBehaviour
 {
     [SerializeField] private NavMeshAgent agent = null;
 
-    private Camera mainCamera;
-
     #region Server
 
+    [ServerCallback]
+    private void Update() {
+
+        if(!agent.hasPath){ return;}
+
+        if(agent.remainingDistance>agent.stoppingDistance){ return;}
+
+        agent.ResetPath();
+    }
+
     [Command]
-    private void CmdMove(Vector3 position)
+    public void CmdMove(Vector3 position)
     {
         if (!NavMesh.SamplePosition(position, out NavMeshHit hit, 1f, NavMesh.AllAreas)) { return; }
 
         agent.SetDestination(hit.position);
-    }
-
-    #endregion
-
-    #region Client
-
-    public override void OnStartAuthority()
-    {
-        mainCamera = Camera.main;
-    }
-
-    [ClientCallback]
-    private void Update()
-    {
-        if (!hasAuthority) { return; }
-
-        if (!Mouse.current.rightButton.wasPressedThisFrame) { return; }
-
-        Ray ray = mainCamera.ScreenPointToRay(Mouse.current.position.ReadValue());
-
-        if (!Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity)) { return; }
-
-        CmdMove(hit.point);
     }
 
     #endregion
